333e39bad8ffb854ef67897413acd473cfb8ee5d
elias <eliasls2002@yahoo.de>
2025-10-03 17:58:21 +0200

    We finished week 3

--- src/IBL.py
+++ src/IBL.py
@@ -1,47 +1,77 @@
 from sklearn.datasets import fetch_openml
+import pandas as pd
 import numpy as np
+
 
 class IBL:
     def __init__(self):
-        a = 1
-    
+        pass
+
     def fit(self, X, y):
-        self.X = X
-        self.y = y
+        self.X = X.reset_index(drop=True)
+        self.y = y.reset_index(drop=True)
 
     def run(self, test_X, k=3, weighing_function='none'):
-        for instance in test_X:
-            distances = self._euclidian_distance(self.X, instance)
-            k_nearest_instances = distances.sort()[:k]
-            if weighing_function == "bameninghong":
-                print("bameninghong")
-            
+        predictions = []
 
-    def _euclidian_distance(self, X, instance):
-        """ Returns Distances to all other instances and the same one is inf """
-        numeric_columns = X.select_dtypes(include=["number"])
-        for index, row in numeric_columns.iterrows():
-            print(row)
-            break
+        for i, instance in test_X.iterrows():
+            distances = self._euclidean_distance(self.X, instance)
 
+            # Sort by distance and get k nearest
+            k_nearest = distances.nsmallest(k, "Distance")
 
+            # Majority voting (basic)
+            neighbor_labels = self.y.loc[k_nearest["Index"]]
+            print(self.X.loc[k_nearest["Index"]], self.y.loc[k_nearest["Index"]])
+
+            if weighing_function == "none":
+                pred = neighbor_labels.mode().iloc[0]
+            else:
+                # Placeholder for weighted voting
+                pred = neighbor_labels.mode().iloc[0]
+
+            predictions.append(pred)
+
+        return predictions
+
+    def _euclidean_distance(self, X, instance):
+        """Compute Euclidean distance of one instance to all in X"""
+        distances = []
+        for index, row in X.iterrows():
+            d = 0.0
+            for idx, value in enumerate(row.values):
+                test_val = instance.iloc[idx]
+
+                # Skip missing values
+                if pd.isna(value) or pd.isna(test_val):
+                    continue
+
+                # Numeric distance
+                if np.issubdtype(type(value), np.number):
+                    d += (value - test_val) ** 2
+                else:
+                    # Categorical mismatch
+                    d += 1
+
+            d = np.sqrt(d)
+            distances.append((index, d))
+
+        return pd.DataFrame(distances, columns=["Index", "Distance"])
 
 
 if __name__ == "__main__":
-
+    # Load Titanic dataset from OpenML
     titanic = fetch_openml(name="titanic", version=1, as_frame=True)
 
     X = titanic.get('data')[:1000]
     y = titanic.get('target')[:1000]
-    X_test = titanic.get('data')[1000:]
-    y_test = titanic.get('target')[1000:]
+    X_test = titanic.get('data')[1100:1110]  # smaller test for demo
+    y_test = titanic.get('target')[1100:1110]
 
     # Testing IBL
     ibl = IBL()
     ibl.fit(X, y)
-    ibl.run(X_test)
+    preds = ibl.run(X_test, k=3)
 
-    
-
-
-        
+    print("Predictions:", preds)
+    print("Ground truth:", list(y_test))
